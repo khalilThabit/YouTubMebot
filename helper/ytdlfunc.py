1debab9475a40a5b5a37de376bb5b626ebf524dc
khalilThabit <88583991+khalilThabit@users.noreply.github.com>
2021-08-08 18:03:31 +0300

    Update ytdlfunc.py

--- helper/ytdlfunc.py
+++ helper/ytdlfunc.py
@@ -48,7 +48,7 @@ def extractYt(yturl):
 #         ydl.download([url])
 
 
-# https://github.com/SpEcHiDe/AnyDLBot
+# https://github.com/khalilThabit/YouTubMebot
 
 async def downloadvideocli(command_to_exec):
     process = await asyncio.create_subprocess_exec(
